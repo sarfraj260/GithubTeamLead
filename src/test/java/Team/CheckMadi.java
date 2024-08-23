8ceb74acb44982b8e2a421acef1d69c7aa0bab81
sarfraj260 <alinoushad260@gmail.com>
2024-08-23 09:40:00 +0530

    massage thursday

--- src/test/java/Team/CheckMadi.java
+++ src/test/java/Team/CheckMadi.java
@@ -5,5 +5,6 @@ public class CheckMadi {
 	public static void main(String[] args) {
 		System.out.println("Team 10:38");
 		System.out.println("massage 11:17");
+		System.out.println("massage thursday  11:17");
 	}
 }
